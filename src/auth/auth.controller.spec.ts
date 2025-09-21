64d6286ebb2c84f386841b99756e3dbbaa654a72
25533582 <63555173+25533582@users.noreply.github.com>
2025-09-21 14:29:01 +0100

    ll

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,22 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
-  let serviceMock: any;
+  let usersServiceMock: any;
+  let authServiceMock: any;
 
   beforeEach(async () => {
-    serviceMock = {
+    usersServiceMock = {
       findAll: jest.fn(),
       findOne: jest.fn(),
       create: jest.fn(),
       remove: jest.fn(),
     };
+
+    authServiceMock = {
+      login: jest.fn(),
+      register: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
-        { provide: UsersService, useValue: serviceMock },
+        { provide: UsersService, useValue: usersServiceMock },
+        { provide: AuthService, useValue: authServiceMock },
       ],
     }).compile();
 
